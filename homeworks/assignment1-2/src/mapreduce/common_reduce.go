1b1bffacb80b8203da2f7428749388c431fe9350
JinLingChristopher <jinl1037@hotmail.com>
2020-01-11 21:14:50 +0800

    add new files.

--- homeworks/assignment1-2/src/mapreduce/common_reduce.go
+++ homeworks/assignment1-2/src/mapreduce/common_reduce.go
@@ -1,15 +1,38 @@
 package mapreduce
 
+import "os"
+
+import "encoding/json"
+
 // doReduce does the job of a reduce worker: it reads the intermediate
 // key/value pairs (produced by the map phase) for this task, sorts the
 // intermediate key/value pairs by key, calls the user-defined reduce function
 // (reduceF) for each key, and writes the output to disk.
+
+func collectReduce(reduceFile)
+
 func doReduce(
 	jobName string, // the name of the whole MapReduce job
 	reduceTaskNumber int, // which reduce task this is
 	nMap int, // the number of map tasks that were run ("M" in the paper)
 	reduceF func(key string, values []string) string,
 ) {
+
+	records := make(map[string][]string)
+	for i := 0; i < nMap; i++ {
+		filename := reduceName(jobName, i, reduceTaskNumber)
+		f, err := os.Open(filename)
+		checkError(err)
+		defer f.Close()
+
+		var kv KeyValue
+		for decoder := json.NewDecoder(f); decoder.More(); {
+			err := decoder.Decode(&kv)
+			checkError(err)
+			records[kv.Key] = append(records[kv.Key], kv.Value)
+		}
+	}
+
 	// TODO:
 	// You will need to write this function.
 	// You can find the intermediate file for this reduce task from map task number
