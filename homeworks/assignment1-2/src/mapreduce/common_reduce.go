5c3f23960cd5230ee6ec0b43f3e715bbad55956e
JinLingChristopher <jinl1037@hotmail.com>
2020-01-11 22:34:54 +0800

    add mr first implementation, more test required.

--- homeworks/assignment1-2/src/mapreduce/common_reduce.go
+++ homeworks/assignment1-2/src/mapreduce/common_reduce.go
@@ -9,15 +9,12 @@ import "encoding/json"
 // intermediate key/value pairs by key, calls the user-defined reduce function
 // (reduceF) for each key, and writes the output to disk.
 
-func collectReduce(reduceFile)
-
 func doReduce(
 	jobName string, // the name of the whole MapReduce job
 	reduceTaskNumber int, // which reduce task this is
 	nMap int, // the number of map tasks that were run ("M" in the paper)
 	reduceF func(key string, values []string) string,
 ) {
-
 	records := make(map[string][]string)
 	for i := 0; i < nMap; i++ {
 		filename := reduceName(jobName, i, reduceTaskNumber)
@@ -33,27 +30,12 @@ func doReduce(
 		}
 	}
 
-	// TODO:
-	// You will need to write this function.
-	// You can find the intermediate file for this reduce task from map task number
-	// m using reduceName(jobName, m, reduceTaskNumber).
-	// Remember that you've encoded the values in the intermediate files, so you
-	// will need to decode them. If you chose to use JSON, you can read out
-	// multiple decoded values by creating a decoder, and then repeatedly calling
-	// .Decode() on it until Decode() returns an error.
-	//
-	// You should write the reduced output in as JSON encoded KeyValue
-	// objects to a file named mergeName(jobName, reduceTaskNumber). We require
-	// you to use JSON here because that is what the merger than combines the
-	// output from all the reduce tasks expects. There is nothing "special" about
-	// JSON -- it is just the marshalling format we chose to use. It will look
-	// something like this:
-	//
-	// enc := json.NewEncoder(mergeFile)
-	// for key in ... {
-	// 	enc.Encode(KeyValue{key, reduceF(...)})
-	// }
-	// file.Close()
-	//
-	// Use checkError to handle errors.
+	mf, err := os.Create(mergeName(jobName, reduceTaskNumber))
+	checkError(err)
+	defer mf.Close()
+
+	enc := json.NewEncoder(mf)
+	for k, v := range records {
+		enc.Encode(KeyValue{k, reduceF(k, v)})
+	}
 }
